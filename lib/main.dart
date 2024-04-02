b09698ecec1af59d32a39557ab86247030b4d056
marco.moreira <marco.moreira@JSEL12D55>
2024-04-02 20:31:09 -0300

    inicail release

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main(){
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // Adicione o parâmetro key e corrija o uso do super
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const WebViewDemo(),
+    );
+  }
+}
+
+
+class WebViewDemo extends StatefulWidget {
+ const WebViewDemo({super.key});
+
+ @override
+ State<WebViewDemo> createState() => _WebViewDemoState();
+}
+
+class _WebViewDemoState extends State<WebViewDemo> {
+ WebViewController controller = WebViewController();
+
+@override
+void initState(){
+super.initState();
+ controller
+ ..setJavaScriptMode(JavaScriptMode.unrestricted)
+ ..setBackgroundColor(const Color(0x000000))
+ ..setNavigationDelegate(NavigationDelegate(
+ onProgress: (progress) => {},
+ onPageFinished: (url) => {},
+ ))
+ ..loadRequest(Uri.parse('http://localhost:8080/login'));
+}
+
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+backgroundColor: Colors.white,
+ appBar: AppBar(
+ title: const Text('demo'),
+ ),
+       body: WebViewWidget(controller: controller,),
+ );
+}
+}
